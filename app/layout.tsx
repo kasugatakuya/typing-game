83d988006d9082fff107fda4266cc6e34013db2d
‘Kasuga_Takuya’ <kasuga0403.biz@gmail.com>
2025-01-30 16:16:24 +0900

    メタタグの設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,14 +11,39 @@ export const metadata: Metadata = {
     "地理タイピングは、タイピング練習をしながら地理を学ぶことができます。",
   keywords:
     "地理タイピング, タイピング, ゲーム, 人気, 国, 都道府県, 首都, 県庁所在地, 世界遺産, 国旗, 地理, 学習, 地球, オンライン, 無料, 練習",
+
+  metadataBase: new URL("https://typing-game-rho-weld.vercel.app"),
+
   openGraph: {
     title: "地理タイピング",
     description:
       "地理タイピングは、タイピング練習をしながら地理を学ぶことができます。",
     type: "website",
+    siteName: "地理タイピング", // これを追加
+    url: "https://typing-game-rho-weld.vercel.app", // これを追加
   },
+
+  alternates: {
+    canonical: "https://typing-game-rho-weld.vercel.app",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+
   icons: {
     icon: "/earth.png",
+  },
+
+  // verification を追加（Google Search Consoleを使用している場合）
+  // Google Search Console の設定→所有権の確認→HTMLタグのコードのcontent属性の値を指定
+  verification: {
+    google: "r4vs5K6kyRpFbT2eQ3xlFxlQbUmRrk_8z6irkNv4HmM",
   },
 };
 
